05be5ef5e5e9e82891720639f5fc1eb3060680da
EiliaD <eiliadastmalchian@crescetnschool.org>
2019-05-19 15:51:09 -0400

    instructions added

--- src/application/Assignment.java
+++ src/application/Assignment.java
@@ -4,6 +4,7 @@ public class Assignment extends Task{
 	private boolean priority;
 	private String subject;
 	
+	//Non-default constructor
 	public Assignment(String name, String description, String subject, boolean priority, int year, int month, int day) {
 		this.name = name;
 		this.description = description;
@@ -12,25 +13,31 @@ public class Assignment extends Task{
 		this.date = new Date(year, month, day);
 	}
 	
-	public void setSubject(String subject) {// sets the subject
+	//Mutator method for the subject
+	public void setSubject(String subject) {
 		this.subject = subject;
 	}
-	public String getSubject() {// gets the subject
+	//Accessor method for the subject
+	public String getSubject() {
 		return subject;
 	}
 	
-	public void setPriority(boolean priority){// sets the priority
+	//Mutator method for the priority
+	public void setPriority(boolean priority){
 		this.priority = priority;
 	}
-	public boolean getPriority() {// gets the priority
+	
+	//Accessor method for the priority
+	public boolean getPriority() {
 		return priority;
 	}
 	
-
-	@Override
-	public boolean isAssignment() {// always returns true since it is an assignmnet
+	//Used to determine whether the object is of type Assignment or Event.
+	public boolean isAssignment() {
 		return true;
 	}
+	
+	//The formatting for quick view used when displaying the tasks in the list
 	public String toString(){
 		return "Name: "+name +"   Date: "+date.getDate();
 	}
--- src/application/DisplayList.java
+++ src/application/DisplayList.java
@@ -10,6 +10,7 @@ import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
   
 public class DisplayList {
+	//All the necessary initializations
 	public static ArrayList<Button> list = new ArrayList<>();
 	public static Pane pane;
 	static Button addTask;
@@ -25,54 +26,65 @@ public class DisplayList {
 	static int buttonIndex;
 	static Rectangle top;
 	
-
+	//Adds a new button to the arraylist and sets the style 
 	public static void addButton(Button a) {
 		a.setStyle("-fx-border-color: #aaaaaa; -fx-border-width: 1px; -fx-background-color: #5e5e5e;");
 		list.add(a);
 	}
 	
+	//Creates a button to add to the arraylist with a label based on the task parameter
 	public static void addTask(Task t){
 		Button newTask = new Button(t.toString());
 		addButton(newTask);
 	}
 	
 	
-
 	public static void setup() {
+		//Sets up the two buttons used to create new events and assignments
 		newEvent.setPrefSize(140, 30);
 		newEvent.setStyle("-fx-border-color: #aaaaaa; -fx-border-width: 1px; -fx-background-color: #5e5e5e;");
+		newEvent.setTranslateX(3);
+		newEvent.setTranslateY(100);
 		newAssignment.setPrefSize(140, 30);
 		newAssignment.setStyle("-fx-border-color: #aaaaaa; -fx-border-width: 1px; -fx-background-color: #5e5e5e;");
 		newAssignment.setTranslateX(3);
 		newAssignment.setTranslateY(65);
 		
+		//Sets up the button that switches the view from list to instructions
 		toInstructions.setStyle("-fx-border-color: #000000; -fx-border-width: 1px; -fx-background-color: #5e5e5e;");
 		toInstructions.setPrefHeight(60);
 		toInstructions.setTranslateX(250);
 		
-		newEvent.setTranslateX(3);
-		newEvent.setTranslateY(100);
-		
+		//Sets up the pane colour as well as the button used to switch from list to calendar
 		pane.setStyle("-fx-border-color: #aaaaaa; -fx-border-width: 1px; -fx-background-color: #999999;");
-		addTask = new Button("New Task");
 		Font f1 = new Font(20);
 		calendarView.setFont(f1);
 		calendarView.setPrefHeight(60);
 		calendarView.setStyle("-fx-border-color: #303030; -fx-border-width: 1px; -fx-background-color: #5e5e5e;");
 		calendarView.setTranslateX(510);
+		
+		//The two buttons that show up when you want to create a new task. They each bring up different menus
 		Font fo = new Font(18);
 		assignment.setStyle("-fx-border-color: #aaaaaa; -fx-border-width: 1px; -fx-background-color: #5e5e5e;");
 		assignment.setFont(fo);
 		event.setStyle("-fx-border-color: #aaaaaa; -fx-border-width: 1px; -fx-background-color: #5e5e5e;");
 		event.setFont(fo);
 
-		
+		//Sets up the button used to let you choose whether you want to create a new assignment or event 
+		addTask = new Button("New Task");
 		addTask.setPrefSize(150, 60);
 		addTask.setStyle("-fx-border-color: #303030; -fx-border-width: 1px; -fx-background-color: #5e5e5e;");
 
+		//The top border of the pane
 		top = new Rectangle(700, 60);
 		top.setFill(Color.rgb(96, 96, 96));
+		
+		//Adds all the nodes that were set up to the pane
 		pane.getChildren().addAll(top, addTask, calendarView, toInstructions);
+		
+		
+		/*The event handler that determines whether to bring up the "newEvent" and "newAssignment" buttons, 
+		the condition being whether or not they already exist*/
 		addTask.setOnMouseClicked((e) -> {
 
 			if(!(pane.getChildren().contains(newAssignment) && pane.getChildren().contains(newEvent))) {
@@ -86,11 +98,17 @@ public class DisplayList {
 	
 	}
 	
+	//The method that displays the arraylist of buttons on the list view. 
 	public static void displayTasks() {
+		
+		//Loops through every button in the arraylist
 		for (int i = 0; i < list.size(); i++) {
+			//Sets the size and position of each button. The vertical position is based on how many buttons there are before it
 			list.get(i).setPrefSize(680, 100);
 			list.get(i).setTranslateX(10);
 			list.get(i).setTranslateY(65 + (i * 100));
+			
+			//Determines whether to change the border of the button to red, depending on the priority of the assignment it represents
 			if( List.list.get(i).isAssignment() && ((Assignment)List.list.get(i)).getPriority()){				
 				list.get(i).setStyle("-fx-border-color: #990000; -fx-border-width: 5px; -fx-background-color: #5e5e5e;");
 			}
@@ -98,28 +116,39 @@ public class DisplayList {
 			list.get(i).setStyle("-fx-border-color: #303030; -fx-border-width: 1px; -fx-background-color: #5e5e5e;");
 			}
 			
+			//Sets the font size of the button
 			Font fo = new Font(25);
 			list.get(i).setFont(fo);
+			
+			//If the button does not already exist, add it
 			if(pane.getChildren().contains(list.get(i)) == false){
 				pane.getChildren().add(list.get(i));
 			}
 			
-
+			//This is to ensure that the top rectangle and all of the buttons on it remain at the top when scrolling down on the list
 			pane.getChildren().removeAll(top, addTask, calendarView, toInstructions);
 			pane.getChildren().addAll(top, addTask, calendarView, toInstructions);
 
+			//The next two blocks of code are used to bring up the correct menu depending on whether an assignment or an event was pressed
+			
+			//If the button represents an assignment, this code runs. 
 			if(List.list.get(i).isAssignment()) {
-			int count = i;
-			String name = List.list.get(i).name;
-			String description = List.list.get(i).description;
-			String subject = ((Assignment)(List.list.get(i))).getSubject();
-			boolean highPri = ((Assignment)(List.list.get(i))).getPriority();
-			Date date = List.list.get(i).date;
-			int year = date.year;
-			int month = date.month;
-			int day = date.day; 
-			list.get(i).setOnAction(e ->{
-				DisplayTask.newThing = false;
+				/*Initializes the name, subject, description, date, and priority of the assignment to be used in the eventhandler,
+				where they will be pre-displayed in the menu that pops up*/
+				int count = i;
+				String name = List.list.get(i).name;
+				String description = List.list.get(i).description;
+				String subject = ((Assignment)(List.list.get(i))).getSubject();
+				boolean highPri = ((Assignment)(List.list.get(i))).getPriority();
+				Date date = List.list.get(i).date;
+				int year = date.year;
+				int month = date.month;
+				int day = date.day; 
+				
+				//The actual eventhandler for when the button is pressed
+				list.get(i).setOnAction(e ->{
+					//The code in here fills out the 
+					DisplayTask.newThing = false;
 					buttonIndex = count;
 					Main.liststack.getChildren().add(DisplayTask.newAssignment);
 					DisplayTask.assignmentname.setText(name);
@@ -129,7 +158,7 @@ public class DisplayList {
 					DisplayTask.regular.setSelected(!highPri);
 					DisplayTask.highPriority = highPri;
 					DisplayTask.dateAssignment.setValue(LocalDate.of(year,month,day)); 
-			});
+				});
 			}
 			
 			else {
